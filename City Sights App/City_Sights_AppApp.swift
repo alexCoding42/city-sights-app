0f21c76f36a3d57ee304830a0ac91acc9f670ac3
alexCoding42 <alexandre.cisse.ac@gmail.com>
2023-04-06 11:47:58 +0700

    set up folders and mvvm

--- City Sights App/City_Sights_AppApp.swift
+++ City Sights App/City_Sights_AppApp.swift
@@ -1,5 +1,5 @@
 //
-//  City_Sights_AppApp.swift
+//  City_Sights_App.swift
 //  City Sights App
 //
 //  Created by Alexandre Cisse on 06/04/2023.
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct City_Sights_AppApp: App {
+struct City_Sights_App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LaunchView()
         }
     }
 }
