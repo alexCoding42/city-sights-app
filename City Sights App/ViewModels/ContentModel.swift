fa581d530731444e2ec27ccacab23f78b0a8da1f
alexCoding42 <alexandre.cisse.ac@gmail.com>
2023-04-12 13:52:56 +0700

    Fetch Yelp API

--- City Sights App/ViewModels/ContentModel.swift
+++ City Sights App/ViewModels/ContentModel.swift
@@ -41,11 +41,49 @@ class ContentModel: NSObject, CLLocationManagerDelegate, ObservableObject {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
         // Give us the location of the user
-        print(locations.first ?? "no location")
+        let userLocation = locations.first
         
-        // Stop requesting the location after we get it once
-        locationManager.stopUpdatingLocation()
+        if userLocation != nil {
+            // Stop requesting the location after we get it once
+            locationManager.stopUpdatingLocation()
+            
+            // TODO: If we have the coordinates of the user, send into Yelp API
+            getBusinesses(category: "restaurants", location: userLocation!)
+            
+        }
+    }
+    
+    // MARK: Yelp API methods
+    
+    func getBusinesses(category: String, location: CLLocation) {
         
-        // TODO: If we have the coordinates of the user, send into Yelp API
+        var urlComponents = URLComponents(string: "https://api.yelp.com/v3/businesses/search")
+        urlComponents?.queryItems = [
+            URLQueryItem(name: "latitude", value: String(location.coordinate.latitude)),
+            URLQueryItem(name: "longitude", value: String(location.coordinate.longitude)),
+            URLQueryItem(name: "categories", value: category),
+            URLQueryItem(name: "limit", value: "6")
+        ]
+        
+        let url = urlComponents?.url
+        
+        if let url = url {
+            
+            var request = URLRequest(url: url, cachePolicy: .reloadIgnoringCacheData, timeoutInterval: 10.0)
+            request.httpMethod = "GET"
+            // Replace with your own Yelp API Key
+            request.addValue("Bearer \(Constants.apiKey)",
+                             forHTTPHeaderField: "Authorization")
+            
+            let session = URLSession.shared
+            
+            let dataTask = session.dataTask(with: request) { (data, response, error) in
+                
+                if error == nil {
+                    print(response)
+                }
+            }
+            dataTask.resume()
+        }
     }
 }
